fd47c247792670d947118d37d246bf221e851fe6
Marcin Lassota <mlassota@jpembedded.eu>
2019-10-30 15:10:57 +0100

    Update ST_xxxx_G215_Interface_Test test case

--- Target/ST_xxxx_G215_Interface_Test.sh
+++ Target/ST_xxxx_G215_Interface_Test.sh
@@ -26,56 +26,40 @@ TestCaseStep2=0 # Cable test
 TestCaseStep3=0 # Cable test
 TestCaseStep4=${ERR_UNDEFINED}
 TestCaseStep5=${ERR_UNDEFINED}
-TestCaseStep6=${ERR_UNDEFINED}
-TestCaseStep7=${ERR_UNDEFINED}
-TestCaseStep8=${ERR_UNDEFINED}
-TestCaseStep9=${ERR_UNDEFINED}
 
 CmdResult=${ERR_UNDEFINED}
 
 # Board in this Test Case always have
-VenID="0x1a88"   
+VenID="0x1a88"
 DevID="0x4d45"
 SubVenID="0x00a2"
 BoardInSystem="1" # Depends on the test case
 InputToChange=${IN_0_ENABLE}
 
 # State machine runs all steps described in Test Case
-# Step1 
+# Step1
 # .....
-# Step9
+# Step5
 # Additional Break state is added to handle/finish TestCase properly
-MachineState="Step1" 
-MachineRun=true 
+MachineState="Step1"
+MachineRun=true
 
-echo "Test case ${ScriptName} started"    
+run_test_case_dir_create ${TestCaseLogName} ${TestCaseName}
+CmdResult=$?
+if [ ${CmdResult} -ne ${ERR_OK} ]; then
+        echo "run_test_case_dir_create: Failed, exit Test Case"
+        exit ${CmdResult}
+else
+        echo "run_test_case_dir_create: Success"
+fi
 
 while ${MachineRun}; do
         case ${MachineState} in
         Step1);&
         Step2);&
-        Step3);&
-        Step4);&
-        Step5);&
-        Step6)  
-                # Run step @4, @5, @6
-                run_test_case_common_actions ${TestCaseLogName} ${TestCaseName}
-                CmdResult=$?
-                if [ ${CmdResult} -ne ${ERR_OK} ]; then
-                        echo "run_test_case_common_actions: Failed, force exit Test Case" | tee -a ${TestCaseLogName} 2>&1
-                        MachineState="Break"
-                else
-                        echo "Run steps @4, @5, @6" > ${TestCaseLogName} 2>&1
-                        echo "Test case ${ScriptName} started" > ${TestCaseLogName} 2>&1
-                        TestCaseStep4=0;
-                        TestCaseStep5=0;
-                        TestCaseStep6=0;
-                        MachineState="Step7"
-                fi
-                ;;
-        Step7)
+        Step3)
                 # Check if mcv_pci is already in blacklist, UART loopback test
-                echo "Run step @7" | tee -a ${TestCaseLogName} 2>&1
+                echo "Run step @3" | tee -a ${TestCaseLogName} 2>&1
                 echo ${MenPcPassword} | sudo -S --prompt= grep "blacklist mcb_pci" /etc/modprobe.d/blacklist.conf > /dev/null
                 if [ $? -ne 0 ]; then
                         # Add mcb_pci into blacklist
@@ -93,13 +77,13 @@ while ${MachineRun}; do
                          echo "uart_test_board success "\
                            | tee -a ${TestCaseLogName} 2>&1
                 fi
-                TestCaseStep7=${CmdResult}
-                MachineState="Step8"
+                TestCaseStep3=${CmdResult}
+                MachineState="Step4"
                 ;;
-        Step8)
+        Step4)
                 # Can test
-                echo "Run step @8" | tee -a ${TestCaseLogName} 2>&1
-                # Run step @8 Test CAN interfaces, there should be 2 cans available
+                echo "Run step @4" | tee -a ${TestCaseLogName} 2>&1
+                # Run step @4 Test CAN interfaces, there should be 2 cans available
                 MezzChamDevName="MezzChamDevName.txt"
                 obtain_device_list_chameleon_device ${VenID} ${DevID} ${SubVenID} ${MezzChamDevName}
 
@@ -112,12 +96,12 @@ while ${MachineRun}; do
                          echo "can_test_ll_z15 success "\
                            | tee -a ${TestCaseLogName} 2>&1
                 fi
-                TestCaseStep8=${CmdResult}
-                MachineState="Step9"
+                TestCaseStep4=${CmdResult}
+                MachineState="Step5"
                 ;;
-        Step9)
-                # Test GPIO / LEDS 
-                echo "Run step @9" | tee -a ${TestCaseLogName} 2>&1
+        Step5)
+                # Test GPIO / LEDS
+                echo "Run step @5" | tee -a ${TestCaseLogName} 2>&1
                 echo ${MenPcPassword} | sudo -S --prompt= modprobe men_ll_z17
                 ResultModprobeZ17=$?
                 if [ ${ResultModprobeZ17} -ne ${ERR_OK} ]; then
@@ -133,10 +117,10 @@ while ${MachineRun}; do
                                 GPIO2=$(grep "^gpio" ${MezzChamDevName} | awk NR==2'{print $1}')
                         fi
 
-                        # Test LEDS -- This cannot be checked automatically yet ... 
+                        # Test LEDS -- This cannot be checked automatically yet ...
                         echo ${MenPcPassword} | sudo -S --prompt= z17_simp ${GPIO1} >> z17_simp_${GPIO1}.txt 2>&1
                         if [ $? -ne 0 ]; then
-                                echo "ERR_RUN :could not run z17_simp ${GPIO1}" | tee -a ${TestCaseLogName} 2>&1     
+                                echo "ERR_RUN :could not run z17_simp ${GPIO1}" | tee -a ${TestCaseLogName} 2>&1
                         fi
 
                         # Test GPIO
@@ -150,10 +134,10 @@ while ${MachineRun}; do
                                    | tee -a ${TestCaseLogName} 2>&1
                         fi
                 fi
-                TestCaseStep9=${CmdResult}
+                TestCaseStep5=${CmdResult}
                 MachineState="Break"
                 ;;
-        Break) 
+        Break)
                 # Clean after Test Case
                 echo "Break State"  | tee --a ${TestCaseLogName}
                 run_test_case_common_end_actions ${TestCaseLogName} ${TestCaseName}
@@ -171,16 +155,12 @@ done
 #echo ${MenPcPassword} | sudo -S --prompt= sed -i '/blacklist mcb_pci/d' /etc/modprobe.d/blacklist.conf
 
 ResultsSummaryTmp="${ResultsFileLogName}.tmp"
-echo "${TestCaseName}    " | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1 
+echo "${TestCaseName}    " | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
 echo "@1 - ${TestCaseStep1}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
 echo "@2 - ${TestCaseStep2}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
 echo "@3 - ${TestCaseStep3}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
 echo "@4 - ${TestCaseStep4}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
 echo "@5 - ${TestCaseStep5}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
-echo "@6 - ${TestCaseStep6}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
-echo "@7 - ${TestCaseStep7}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
-echo "@8 - ${TestCaseStep8}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
-echo "@9 - ${TestCaseStep9}" | tee -a ${TestCaseLogName} ${ResultsSummaryTmp} 2>&1
 
 # Clean after Test Case
 run_test_case_common_end_actions ${TestCaseLogName} ${TestCaseName}
