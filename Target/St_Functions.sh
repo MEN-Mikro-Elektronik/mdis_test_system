ad446b4fbe4a2f347031efe9d33d4ce3413ae0b1
Marcin Lassota <mlassota@jpembedded.eu>
2019-11-29 17:14:04 +0100

    Fix os name getting function

--- Target/St_Functions.sh
+++ Target/St_Functions.sh
@@ -124,6 +124,8 @@ function get_os_name_with_kernel_ver {
         local Arch=`uname -m`
         cd "${CurrDir}"
         SystemName="${SystemName//\//_}"
+        SystemName="${SystemName//(/_}"
+        SystemName="${SystemName//)/_}"
         echo "${SystemName}_${Kernel}_${Arch}"
 }
 
