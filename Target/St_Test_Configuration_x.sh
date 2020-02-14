580d24f4e2828e481cc2d5bee13f31936470e4c2
KonradJPE <ktomasik@jpembedded.eu>
2020-02-14 12:58:34 +0100

    Minor code fixes

--- Target/St_Test_Configuration_x.sh
+++ Target/St_Test_Configuration_x.sh
@@ -21,52 +21,52 @@ echo "Testing:  ${TestConfiguration}"
 echo "Commit SHA: ${CommitSha}"
 echo "Os Name:  ${OsNameKernel}"
 
-cd "${MdisResultsDirectoryPath}"
+cd "${MdisResultsDirectoryPath}" || exit "${ERR_NOEXIST}"
 
 CommitSha="Commit_${CommitSha}"
 
 create_directory "${CommitSha}"
 CmdResult=$?
-if [ ${CmdResult} -ne "${ERR_OK}" ] && [ ${CmdResult} -ne ${ERR_DIR_EXISTS} ]; then
+if [ ${CmdResult} -ne "${ERR_OK}" ] && [ ${CmdResult} -ne "${ERR_DIR_EXISTS}" ]; then
         exit ${CmdResult}
 fi
-cd "${CommitSha}"
+cd "${CommitSha}" || exit "${ERR_NOEXIST}"
 
 create_directory "${TestConfiguration}"
 CmdResult=$?
-if [ ${CmdResult} -ne ${ERR_OK} ] && [ ${CmdResult} -ne ${ERR_DIR_EXISTS} ]; then
+if [ ${CmdResult} -ne "${ERR_OK}" ] && [ ${CmdResult} -ne "${ERR_DIR_EXISTS}" ]; then
         exit ${CmdResult}
 fi
-cd "${TestConfiguration}"
+cd "${TestConfiguration}" || exit "${ERR_NOEXIST}"
 
-create_directory "${Today}"
+create_directory "${Today}" || exit "${ERR_NOEXIST}"
 CmdResult=$?
-if [ ${CmdResult} -ne ${ERR_OK} ] && [ ${CmdResult} -ne ${ERR_DIR_EXISTS} ]; then
+if [ ${CmdResult} -ne "${ERR_OK}" ] && [ ${CmdResult} -ne "${ERR_DIR_EXISTS}" ]; then
         exit ${CmdResult}
 fi
-cd "${Today}"
+cd "${Today}" || exit "${ERR_NOEXIST}"
 
 OsNameKernel=$(echo "${OsNameKernel}" | tr -dc '[:alnum:]')
 create_directory "${OsNameKernel}"
 CmdResult=$?
-if [ ${CmdResult} -ne ${ERR_OK} ] && [ ${CmdResult} -ne ${ERR_DIR_EXISTS} ]; then
+if [ ${CmdResult} -ne "${ERR_OK}" ] && [ ${CmdResult} -ne "${ERR_DIR_EXISTS}" ]; then
         exit ${CmdResult}
 fi
-cd "${OsNameKernel}"
+cd "${OsNameKernel}" || exit "${ERR_NOEXIST}"
 
 TestSummaryDirectory="${MdisResultsDirectoryPath}/${CommitSha}/${TestConfiguration}/${Today}/${OsNameKernel}"
-cd "${MainTestDirectoryPath}"
+cd "${MainTestDirectoryPath}" || exit "${ERR_NOEXIST}"
 
-mdis_prepare ${TestSummaryDirectory}
+mdis_prepare "${TestSummaryDirectory}"
 CmdResult=$?
-if [ ${CmdResult} -ne ${ERR_OK} ]; then
+if [ ${CmdResult} -ne "${ERR_OK}" ]; then
         echo "run_test_case_common_actions: Failed - exit"
         exit ${CmdResult}
 else
         echo "run_test_case_common_actions: Success"
 fi
 
-echo ${1} | sudo -S --prompt=$'\r' dmesg --clear
+echo "${1}" | sudo -S --prompt=$'\r' dmesg --clear
 
 echo "Test Setup: ${TestSetup}"
 case ${TestSetup} in
@@ -77,35 +77,35 @@ case ${TestSetup} in
           #echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G215_Interface_Test.sh" ${TestSummaryDirectory}
           ;;
         2)
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_F205_M57_M_Module_Test.sh" ${TestSummaryDirectory} "1"
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M32_M58_M_Module_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M37_M62_M_Module_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_F223_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M66_M31_M_Module_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M43_M11_M_Module_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_SMB2_Test.sh" ${TestSummaryDirectory} "smb2_1" "F026L00"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_F205_M57_M_Module_Test.sh" "${TestSummaryDirectory}" "1"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M32_M58_M_Module_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M37_M62_M_Module_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_F223_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M66_M31_M_Module_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_M43_M11_M_Module_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_SMB2_Test.sh" "${TestSummaryDirectory}" "smb2_1" "F026L00"
           ;;
         3)
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M77_M_Module_Test.sh" ${TestSummaryDirectory} "1" "3"
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M82_M_Module_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M36N_M_Module_Test.sh" ${TestSummaryDirectory}
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M77_M_Module_Test.sh" "${TestSummaryDirectory}" "1" "3"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M82_M_Module_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M36N_M_Module_Test.sh" "${TestSummaryDirectory}"
           #echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M77_M_Module_Test.sh" ${TestSummaryDirectory} "1" "1" #M77Nr, G204Nr
           #echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M82_M_Module_Test.sh" ${TestSummaryDirectory}
           #echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M82_M_Module_Test.sh" ${TestSummaryDirectory} "2" #second device instance
 
           ;;
         4)
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G229_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M77_M_Module_Test.sh" ${TestSummaryDirectory} "1" "1"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G229_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M77_M_Module_Test.sh" "${TestSummaryDirectory}" "1" "1"
           #echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M82_M_Module_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M72_M_Module_Test.sh" ${TestSummaryDirectory}
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_SMB2_Test.sh" ${TestSummaryDirectory} "smb2_1" "G025A03"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M72_M_Module_Test.sh" "${TestSummaryDirectory}"
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_SMB2_Test.sh" "${TestSummaryDirectory}" "smb2_1" "G025A03"
           ;;
         5)
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_BL51E_Test.sh" ${TestSummaryDirectory}
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_BL51E_Test.sh" "${TestSummaryDirectory}"
           ;;
         6)
-          echo ${1} | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M65_M_Module_Test.sh" ${TestSummaryDirectory}
+          echo "${1}" | sudo -S --prompt=$'\r' "${MyDir}/ST_xxxx_G204_M65_M_Module_Test.sh" "${TestSummaryDirectory}"
           ;;
         *)
           echo "TEST SETUP IS NOT SET"
@@ -113,11 +113,11 @@ case ${TestSetup} in
         ;;
 esac
 
-echo ${1} | sudo -S --prompt=$'\r' bash -c "dmesg > dmesg_log.txt"
+echo "${1}" | sudo -S --prompt=$'\r' bash -c "dmesg > dmesg_log.txt"
 
 echo "Create Test Results summary for TestSetup ${TestSetup}"
-cd "${TestSummaryDirectory}"
+cd "${TestSummaryDirectory}" || exit "${ERR_NOEXIST}"
 
 SystemInfo="$(uname -a)"
-find . -type f -name "${ResultsFileLogName}.tmp" -exec cat {} + > ${ResultsFileLogName}
-sed -i "1i${SystemInfo}" ${ResultsFileLogName}
+find . -type f -name "${ResultsFileLogName}.tmp" -exec cat {} + > "${ResultsFileLogName}"
+sed -i "1i${SystemInfo}" "${ResultsFileLogName}"
