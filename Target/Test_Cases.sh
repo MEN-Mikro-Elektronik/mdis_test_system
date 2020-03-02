beb4d9a5ba069663ac456b79829f0ced0c270f79
KonradJPE <ktomasik@jpembedded.eu>
2020-03-02 10:05:03 +0100

    Minor fix

--- Target/Test_Cases.sh
+++ Target/Test_Cases.sh
@@ -17,7 +17,6 @@ function run_test_case {
         0103)
             run_as_root "${MyDir}/Test_x.sh" "${TestSummaryDirectory}" "${TestCaseId}" "${OsNameKernel}" "g229"
             ;;
-            ;;
         0200)
             run_as_root "${MyDir}/Test_x.sh" -dir "${TestSummaryDirectory}" -id "${TestCaseId}" -os "${OsNameKernel}" -dname "carrier_g204_m65n"
             ;;
