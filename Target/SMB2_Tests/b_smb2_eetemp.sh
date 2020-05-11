27c52a4e49b77bcf558eb8c8cd77e05bb1f48b73
Marcin Lassota <mlassota@jpembedded.eu>
2020-05-11 16:15:57 +0200

    Add smb2 temperature reading test case

--- Target/SMB2_Tests/b_smb2_eetemp.sh
+++ Target/SMB2_Tests/b_smb2_eetemp.sh
@@ -19,9 +19,11 @@ function b_smb2_eetemp_description {
     echo "    It is assumed that at this point all necessary drivers have been build and"
     echo "    are available in the system"
     echo "DESCRIPTION:"
-    echo ""
+    echo "    1.Load drivers: modprobe men_mdis_kernel"
+    echo "    2.Get board temperature"
+    echo "    3.Check if temeparatue is correct"
     echo "PURPOSE:"
-    echo ""
+    echo "    Check if board temperature can be read"
     echo "UPPER_REQUIREMENT_ID:"
     print_env_requirements "${TestSummaryDirectory}"
     echo "    MEN_13MD0590_SWR_1950"
@@ -47,5 +49,64 @@ function b_smb2_eetemp_test {
     local LogFile=${4}
     local LogPrefix=${5}
     local BoardInSystem=${6}
+
+    MachineState="Step1"
+    MachineRun=true
+
+    DevName="smb2_1"    # smb device name (e.g. smb2_1)
+
+    while ${MachineRun}; do
+        case ${MachineState} in
+            Step1)
+                debug_print "${LogPrefix} Run step @1" "${LogFile}"
+                if ! run_as_root modprobe men_mdis_kernel
+                then
+                    debug_print "${LogPrefix}  ERR_MODPROBE: could not modprobe men_mdis_kernel" "${LogFile}"
+                    TestCaseStep1=${ERR_MODPROBE}
+                    MachineState="Break"
+                else
+                    TestCaseStep1=${ERR_OK}
+                    MachineState="Step2"
+                fi
+                ;;
+            Step2)
+                debug_print "${LogPrefix} Run step @2" "${LogFile}"
+                if ! run_as_root smb2_eetemp "${DevName}" > smb2_eetemp.log; then
+                    debug_print "${LogPrefix}  ERR_VALUE: Could not get temperature with smb2_eetemp" "${LogFile}"
+                    TestCaseStep2=${ERR_VALUE}
+                    MachineState="Break"
+                else
+                    TestCaseStep2=${ERR_OK}
+                    MachineState="Step3"
+                fi
+                ;;
+            Step3)
+                debug_print "${LogPrefix} Run step @3" "${LogFile}"
+                if ! grep "Current Board temperature: [0-9]\+\.[0-9]\+" smb2_eetemp.log; then
+                    debug_print "${LogPrefix}  ERR_VALUE: could not read temperature" "${LogFile}"
+                    TestCaseStep3=${ERR_VALUE}
+                    MachineState="Break"
+                else
+                    TestCaseStep3=${ERR_OK}
+                    MachineState="Break"
+                fi
+                ;;
+            Break) # Clean after Test Case
+                debug_print "${LogPrefix} Break State" "${LogFile}"
+                MachineRun=false
+                ;;
+            *)
+                debug_print "${LogPrefix} State is not set, start with Step1" "${LogFile}"
+                MachineState="Step1"
+                ;;
+        esac
+    done
+
+    if [ "${TestCaseStep1}" = "${ERR_OK}" ] && [ "${TestCaseStep2}" = "${ERR_OK}" ] &&\
+       [ "${TestCaseStep3}" = "${ERR_OK}" ]; then
+        return "${ERR_OK}"
+    else
+        return "${ERR_VALUE}"
+    fi
 
 }
