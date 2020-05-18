36ede32b6e4d91be98f4bddf0fa83301fd6e43e2
Marcin Lassota <mlassota@jpembedded.eu>
2020-05-18 15:59:35 +0200

    Fix setup configuration 9

--- Target/St_Test_Configuration_x.sh
+++ Target/St_Test_Configuration_x.sh
@@ -186,7 +186,6 @@ echo "${LogPrefix} Test Setup: ${TEST_SETUP}"
             run_test_case_board "2" "1" "${TestSummaryDirectory}" "${OsNameKernel}" # SMB2_TEST @ BL51
             run_test_case_board "3" "1" "${TestSummaryDirectory}" "${OsNameKernel}" # SMB2_TEST @ BL51
             run_test_case_board "4" "1" "${TestSummaryDirectory}" "${OsNameKernel}" # SMB2_TEST @ BL51
-            run_test_case_board "5" "1" "${TestSummaryDirectory}" "${OsNameKernel}" # SMB2_TEST @ BL51
             run_test_case_board "501" "1" "${TestSummaryDirectory}" "${OsNameKernel}" # BL51 fpga ip core tests
             ;;
         10)
