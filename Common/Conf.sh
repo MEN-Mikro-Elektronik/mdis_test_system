853a786032048aa7245966ae34e4bc11be0033ef
Konrad Tomasik <ktomasik@jpembedded.eu>
2020-06-03 09:07:34 +0200

    Update G25 OS list

--- Common/Conf.sh
+++ Common/Conf.sh
@@ -176,10 +176,11 @@ GrubOsesG23=("0" \
         )
 #G25A
 GrubOsesG25A=("0" \
-        "Ubuntu, with Linux 4.15.0-45-generic (on /dev/sda14)" \
-        "Ubuntu, with Linux 4.15.0-45-generic (on /dev/sda15)" \
         "Ubuntu 18.04.3 LTS (18.04) (on /dev/sda16)" \
         "Ubuntu, with Linux 5.0.0-23-generic (on /dev/sda17)" \
+        "Ubuntu, with Linux 5.4.0-26-generic (on /dev/sda12)" \
+        "CentOS Linux 7 (Core) (on /dev/sda10)" \
+        "CentOS Linux 8 (Core) (on /dev/sda11)" \
         "Debian GNU/Linux, with Linux 4.19.0-6-686-pae (on /dev/sda18)" \
         "Debian GNU/Linux, with Linux 4.19.0-6-amd64 (on /dev/sda19)" \
         )
